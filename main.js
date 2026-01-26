681fde2fc6ac6eac9ef8af891556357e861b49c7
Mostafa <memo605.work@gmail.com>
2026-01-26 23:43:10 +0200

    Add all of the winning conditions and tie condition.

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 //Gameboard "canvas"
 const Gameboard = (function (){
 
-const board = ["1","2","3","4","5","6","7","8","9"];
+const board = ["","","","","","","","",""];
 
- console.log(board);
 
  //a placeMark function that takes the index/cell the player wants to update and who is the current player
  function placeMark(currentPlayer,index) {
-    board[index] = currentPlayer
-    console.log(board);
-    
+        board[index] = currentPlayer
+        console.log(board);
  }
- return {placeMark}
+
+ const winnerCondtions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+ ]
+
+    function checkWinner() {
+     for (let pattern of winnerCondtions) {
+     const [a,b,c] = pattern  ;
+     if (
+        board[a]!=="" &&
+        board[a]===board[b]&&
+        board[b]===board[c]) {
+        return true;
+     } 
+ }
+ return false;
+    }
+
+    function checkTie() {
+        return board.every(cell => cell !=="")
+    }
+ 
+ return {placeMark,board,checkWinner, checkTie}
 })();
-
-
 
 
 //Game controller
@@ -24,24 +49,48 @@ const GameController = (function () {
     let playerX = "X";
     let playerO = "O";
     let currentPlayer = playerX;
-    let round = 1;
-    console.log(currentPlayer);
-    
+    let gameOver = false;
     let currentMove = "";
+    
+    console.log(`It's player ${currentPlayer} turn`);
+    
     function switchPlayer() {
        currentPlayer === playerX ? currentPlayer = playerO :currentPlayer =  playerX;
-       console.log(currentPlayer);
-    }    switchPlayer()
+    }    
 
     //a function that takes the move location and checks with the board if the cell is empty or not
     // if yes, it takes the current player and the move location "index" as arguemnts 
     
     const playTurn = (index) =>{
-        Gameboard.placeMark(currentPlayer,index)
+        if (gameOver===false) {
+            if (Gameboard.board[index]==="") {
+        Gameboard.placeMark(currentPlayer,index);
+        if (Gameboard.checkWinner()) {
+              console.log("We have a winner!!!");
+              gameOver = true;
+              return;
+        } else {
+            if (Gameboard.checkTie()) {
+                console.log("It's a tie!");
+                gameOver = true;
+              return;
+            } else {
+                 switchPlayer();
+                 console.log(`It's player ${currentPlayer} turn`);
+            }
+        }
+            
+       }else{
+        console.log("This cell is already occupied, please choose another cell!");
+        console.log(`It's player ${currentPlayer} turn`);
+       }
+        }else {
+            console.log("Game is already over!");
+            return;
+        }
     }
    return {switchPlayer,playTurn}
 })();
-GameController.playTurn(0);
 
 
 
